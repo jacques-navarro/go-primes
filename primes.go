f02dbf7bea1245a26c4cfca44e3ab6637dab1530
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2025-09-14 22:09:41 +0200

    Add IsPrime function implementation

--- primes.go
+++ primes.go
@@ -1,1 +1,28 @@
 package primes
+
+import (
+	"math"
+)
+
+func IsPrime(n int) bool {
+
+	if n == 2 {
+		return true
+	}
+
+	if n%2 == 0 {
+		return false
+	}
+
+	midPoint := int(math.Sqrt(float64(n)))
+
+	for i := 3; i <= midPoint+1; i += 2 {
+
+		if n%i == 0 {
+			return false
+		}
+	}
+
+	return true
+
+}
